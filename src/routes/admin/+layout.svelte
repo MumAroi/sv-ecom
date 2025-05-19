e1739657bc57358722720bd9ce4c1d49198a6e19
Paramas Waewsuwan <paramas_w@tvdirect.tv>
2025-05-19 18:04:48 +0700

    📦 package:  sidebar

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { LayoutDashboard, Box, User, Truck, Blocks, Home } from '@lucide/svelte';
+
+	// Menu items.
+	const items = [
+		{
+			title: 'Overview',
+			url: 'overview',
+			icon: LayoutDashboard
+		},
+		{
+			title: 'Products',
+			url: 'products',
+			icon: Box
+		},
+		{
+			title: 'users',
+			url: 'users',
+			icon: User
+		},
+		{
+			title: 'Orders',
+			url: 'order',
+			icon: Truck
+		},
+		{
+			title: 'Categories',
+			url: 'categories',
+			icon: Blocks
+		},
+		{
+			title: 'Store front',
+			url: '../',
+			icon: Home
+		}
+	];
+
+	const { children } = $props();
+</script>
+
+<Sidebar.Provider>
+	<Sidebar.Root>
+		<Sidebar.Content>
+			<Sidebar.Group>
+				<Sidebar.GroupLabel>Application</Sidebar.GroupLabel>
+				<Sidebar.GroupContent>
+					<Sidebar.Menu>
+						{#each items as item (item.title)}
+							<Sidebar.MenuItem>
+								<Sidebar.MenuButton>
+									{#snippet child({ props })}
+										<a href={`/admin/${item.url}`} {...props}>
+											<item.icon />
+											<span>{item.title}</span>
+										</a>
+									{/snippet}
+								</Sidebar.MenuButton>
+							</Sidebar.MenuItem>
+						{/each}
+					</Sidebar.Menu>
+				</Sidebar.GroupContent>
+			</Sidebar.Group>
+		</Sidebar.Content>
+	</Sidebar.Root>
+	<main class=" flex-1">
+		<Sidebar.Trigger />
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
