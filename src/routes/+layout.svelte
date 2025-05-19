f1d6e049756c7eb4d7dd603447d7ddfe25ff5452
Paramas Waewsuwan <paramas_w@tvdirect.tv>
2025-05-19 17:42:54 +0700

    📦  package: biome lucide nprogress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+	import NProgress from 'nprogress';
+	import { afterNavigate, beforeNavigate } from '$app/navigation';
+	const { children } = $props();
 
-	let { children } = $props();
+	afterNavigate(() => {
+		NProgress.start();
+	});
+
+	beforeNavigate(() => {
+		NProgress.done();
+	});
+
+	NProgress.configure({ showSpinner: false });
 </script>
 
 {@render children()}
