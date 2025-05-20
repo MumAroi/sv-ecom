2770254b0e339daf47546f6ce239e1c71b3b5814
Paramas Waewsuwan <paramas_w@tvdirect.tv>
2025-05-20 21:39:36 +0700

    ✨ feat: header layout

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import * as Avatar from '$lib/components/ui/avatar';
+	import Button from './ui/button/button.svelte';
+	import { Menu, ShoppingCart } from '@lucide/svelte';
+
+	const user = true;
+	const isAdmin = false;
+	const accountPages = [
+		{
+			title: 'Profile',
+			href: '/me/personal-info'
+		},
+		{
+			title: 'Order history',
+			href: '/me/order-history'
+		},
+
+		{
+			title: 'Addresses',
+			href: '/me/addresses'
+		},
+		{
+			title: 'Settings',
+			href: '/me/settings'
+		}
+	];
+
+	function getUserInitials(name: string) {
+		return name
+			.split(' ')
+			.map((n) => n[0])
+			.join('');
+	}
+</script>
+
+<header
+	class="bg-background sticky top-0 left-0 z-50 flex items-center justify-between border px-3 py-3 md:px-10"
+>
+	<a href="/" class="text-2xl capitalize">
+		<span class="text-primary font-bold">My Svelte</span>
+		<span>Store</span>
+	</a>
+	<div class="flex items-center gap-2 md:gap-6">
+		{#if user}
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger>
+					<Avatar.Root>
+						<Avatar.Image src="https://github.com/shadcn.png" alt="User" />
+						<Avatar.Fallback>{getUserInitials('John Doe')}</Avatar.Fallback>
+					</Avatar.Root>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content>
+					<DropdownMenu.Group>
+						<DropdownMenu.Label>My Account</DropdownMenu.Label>
+						<DropdownMenu.Separator />
+						{#each accountPages as { href, title }}
+							<DropdownMenu.Item>
+								{#snippet child({ props })}
+									<a {href} {...props}>{title}</a>
+								{/snippet}
+							</DropdownMenu.Item>
+						{/each}
+						{#if isAdmin}
+							<DropdownMenu.Item>
+								{#snippet child({ props })}
+									<a href="/admin" {...props}>Admin Dashboard</a>
+								{/snippet}
+							</DropdownMenu.Item>
+						{/if}
+						<DropdownMenu.Separator />
+						<DropdownMenu.Item>
+							{#snippet child({ props })}
+								<a href="/logout" {...props}>Logout</a>
+							{/snippet}
+						</DropdownMenu.Item>
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		{:else}
+			<div class="hidden items-center gap-2 md:flex">
+				<Button variant="ghost">Sign In</Button>
+				<Button>Sign Up</Button>
+			</div>
+			<button
+				class="hover:bg-secondary flex size-10 items-center justify-center rounded-full md:hidden"
+			>
+				<Menu class="size-6" />
+			</button>
+		{/if}
+		{#if user}
+			<button class="inline-blox relative h-fit w-fit">
+				<div
+					class="bg-primary absolute -top-2 -right-2 flex size-5 items-center justify-center rounded-full text-xs text-white"
+				>
+					5
+				</div>
+				<ShoppingCart class="size-6" />
+			</button>
+		{/if}
+	</div>
+</header>
---
+++ src/routes/(storeFront)/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Header from '@/components/Header.svelte';
+
+	const { children } = $props();
+</script>
+
+<!-- Header component -->
+<Header />
+
+{@render children()}
